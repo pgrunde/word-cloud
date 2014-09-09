506f9397a44660f8ae7b713fe01faebe9e41b343
Peter Grunde <peter.a.grunde@gmail.com>
2014-09-09 09:58:30 -0600

    Refactored for smaller methods

--- wordcloud.rb
+++ wordcloud.rb
@@ -7,28 +7,7 @@ def read_file(file)
   data
 end
 
-json = '{
-  "Ila Huels": [
-    "OPTIMIZE WEB-ENABLED SUPPLY-CHAINS",
-    "brand sexy channels",
-    "ENVISIONEER ROBUST E-COMMERCE",
-    "TRANSFORM WIRELESS ARCHITECTURES",
-    "BENCHMARK CROSS-PLATFORM PARTNERSHIPS"
-  ],
-  "Cristopher Feest": [
-    "BENCHMARK CROSS-PLATFORM PARTNERSHIPS",
-    "brand sexy channels",
-    "BENCHMARK 24/7 PARADIGMS"
-  ]
-}'
-
-
-data = JSON.parse(json)
-
-def word_cloud(data)
-  output = {}
-  all_words = []
-  # go over each person, pull out each phrase & split by whitespace
+def whitespace_split(data, all_words)
   data.each_value do |v|
     v.each do |phrase|
       individual_words = phrase.split(" ")
@@ -36,12 +15,13 @@ def word_cloud(data)
     end
   end
   all_words.uniq!
-  # take each word, find out if a person said them and how many times.
+end
+
+def build_output(all_words, output, data)
   all_words.each do |word|
     count = 0
     name_arr = []
     data.each_pair do |name,phrases|
-
       phrases.each do |phrase|
         if phrase.downcase.include?(word)
           count += 1
@@ -50,11 +30,18 @@ def word_cloud(data)
           end
         end
       end
-
     end
     output[word] = { :count => count, :people => name_arr}
   end
+end
 
+def word_cloud(data)
+  output = {}
+  all_words = []
+  # go over each person, pull out each phrase & split by whitespace
+  whitespace_split(data, all_words)
+  # take each word, find out if a person said them and how many times.
+  build_output(all_words, output, data)
   output
 end
 
